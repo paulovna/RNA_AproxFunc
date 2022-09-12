ebc3559d75cb33ac71615d380f1c2e813014ea5d
paulovna <60780168+paulovna@users.noreply.github.com>
2022-09-11 21:49:27 -0300

    Update teste4sim2.py

--- teste4sim2.py
+++ teste4sim2.py
@@ -10,7 +10,7 @@ y = np.ravel(arquivo[1])
 arquivo = open("Teste4-Sim2/log.txt",'w')
 arquivo.close()
 
-#Usando o teste de arquitetura qtdd
+#Usando o teste de arquitetura neuronios
 for j in range(0, 10):
 
     regr = MLPRegressor(hidden_layer_sizes=(2000, 800), 
