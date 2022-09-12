6ab6b994d114e202e1805c43c92d7ee42b4b77ca
paulovna <60780168+paulovna@users.noreply.github.com>
2022-09-11 21:48:53 -0300

    Update teste3sim3.py

--- teste3sim3.py
+++ teste3sim3.py
@@ -10,7 +10,7 @@ y = np.ravel(arquivo[1])
 arquivo = open("Teste3-Sim3/log.txt",'w')
 arquivo.close()
 
-#Usando o teste de arquitetura neuronios
+#Usando o teste de arquitetura camadas
 for j in range(0, 10):
 
     regr = MLPRegressor(hidden_layer_sizes=(700, 3000), 
